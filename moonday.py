1e37c9cd4ad7e9f50d3c2742594a0079eb746f67
SONG <simpledeveloper.songs@gmail.com>
2021-08-30 18:15:32 +0900

    줄정리

--- moonday.py
+++ moonday.py
@@ -1,6 +1,7 @@
 from korean_lunar_calendar import KoreanLunarCalendar
 
-def get_luna_date(year,month,day):
+
+def get_luna_date(year, month, day):
     calendar = KoreanLunarCalendar()
     # params : year(년), month(월), day(일)
     calendar.setSolarDate(year, month, day)
@@ -8,5 +9,9 @@ def get_luna_date(year,month,day):
     print("양력>음력", calendar.LunarIsoFormat(), "입니다.")
     return calendar.LunarIsoFormat()
 
+
 if __name__ == "__main__":
-    print(type(get_luna_date(2020,8,8)))
+    print(type(get_luna_date(2020, 8, 8)))
+
+    print(get_luna_date(2020, 8, 8))
+    print(get_luna_date(2020, 8, 8)[8:])
